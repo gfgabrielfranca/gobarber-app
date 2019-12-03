21673b3b94d9007159051f8296ef9350c7c1071e
Gabriel França <dev.gabrielfranca@gmail.com>
2019-12-02 23:51:46 -0300

    style appointments

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment({ data }) {
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${data.provider.name}.png`,
+          }}
+        />
+
+        <Info>
+          <Name>{data.provider.name}</Name>
+          <Time>em 3 horas</Time>
+        </Info>
+      </Left>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" size={20} color="#f64c75" />
+      </TouchableOpacity>
+    </Container>
+  );
+}
